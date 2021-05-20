e90cce88d95fa9ff92484342ad963a0879b79d96
Denis Kuimov <deniskuimov3@mail.com>
2021-05-20 19:40:21 +0500

    deploy firebase

--- для webpack/src/webpack.config.js
+++ для webpack/src/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   plugins: [
     new HTMLPlugin({
       template: './src/assets/templates/pages/index.html'
-    })
+    }),
   ],
   module: {
     rules: [
@@ -23,12 +23,41 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg)$/i,
-        use: ['file-loader'],
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
         use: ['file-loader?name=./src/fonts/fonts/geometria-medium.[ttf]']
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          sources: {
+            list: [
+              // All default supported tags and attributes
+              '...',
+              {
+                tag: 'img',
+                attribute: 'data-src',
+                type: 'src',
+              },
+              {
+                tag: 'img',
+                attribute: 'data-srcset',
+                type: 'srcset',
+              },
+            ],
+            urlFilter: (attribute, value, resourcePath) => {
+              if (/images\.png$/.test(value)) {
+                return false;
+              }
+
+              return true;
+            },
+          },
+        },
       },
     ],
   },
